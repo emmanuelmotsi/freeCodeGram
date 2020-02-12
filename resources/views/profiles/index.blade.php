9cf286da3aeaf128932e70c50bcd4f2dd104bec2
Emmanuel Motsi <emmanuelmotsi@gmail.com>
2020-02-12 19:23:36 +0200

    Addition of new migration for model Post.

--- resources/views/profiles/index.blade.php
+++ resources/views/profiles/index.blade.php
@@ -7,17 +7,18 @@
             <img src="https://instagram.fhre2-1.fna.fbcdn.net/v/t51.2885-19/s150x150/83213956_3360255157381124_5752385570823208960_n.jpg?_nc_ht=instagram.fhre2-1.fna.fbcdn.net&_nc_ohc=XflqF9pLQ-gAX_TPprT&oh=e993666e8013141444b9b06778c49768&oe=5ECEBCBA" class="rounded-circle">
         </div>
         <div class="col-9 pt-5">
-                <div>
-                    <h1>freecodecamp</h1>
+                <div class="d-flex justify-content-between align-items-baseline">
+                    <h1>{{$user->username}}</h1>
+                    <a href="#">Add a post..</a>
                 </div>
                 <div class="d-flex">
                     <div class="pr-5"><strong>153</strong> posts</div>
                     <div class="pr-5"><strong>23k</strong> followers</div>
                     <div class="pr-5"><strong>212k</strong> following</div>
                 </div>
-                <div class="pt-4 font-weight-bold">freeCodeCamp.org</div>
-                <div>We're a global community of millions of people learning to code together. We're an open source, donor-supported, 501(c)(3) nonprofit.</div>
-                <div><a href="https://www.freecodecamp.org">www.freecodecamp.org</a></div>
+                <div class="pt-4 font-weight-bold">{{$user->profile->title}}</div>
+                <div>{{$user->profile->description}}</div>
+            <div><a href="#">{{$user->profile->url}}</a></div>
         </div>
 
     </div>
