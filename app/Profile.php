71e11321a58d6f61f05750e7c7c4ed1e27750983
Emmanuel Motsi <emmanuelmotsi@gmail.com>
2020-02-29 11:35:47 +0200

    Implemented some authorization.

--- app/Profile.php
+++ app/Profile.php
@@ -6,10 +6,17 @@ use Illuminate\Database\Eloquent\Model;
 
 class Profile extends Model
 {
+
+    protected $guarded = [];
+
+    public function profileImage()
+    {
+        $imagePath = $this->image ?: 'profile/R26y86EAFIbXb4ZNWqY6pdvz8Lw65I23tMYiq6dT.png';
+        return '/storage/' . $imagePath;
+    }
+
     public function user() 
     {
         return $this->belongsTo(User::class);
     }
 }
-
-
--- storage/framework/views/d286ff8f1717dcdbdbfc26baa4c247a37ac677ed.php
+++ storage/framework/views/d286ff8f1717dcdbdbfc26baa4c247a37ac677ed.php
@@ -2,15 +2,28 @@
 <div class="container">
     <div class="row">
         <div class="col-3 p-5">
-            <img src="https://instagram.fhre2-1.fna.fbcdn.net/v/t51.2885-19/s150x150/83213956_3360255157381124_5752385570823208960_n.jpg?_nc_ht=instagram.fhre2-1.fna.fbcdn.net&_nc_ohc=XflqF9pLQ-gAX_TPprT&oh=e993666e8013141444b9b06778c49768&oe=5ECEBCBA" class="rounded-circle">
+          <img src="<?php echo e($user->profile->profileImage()); ?>" class="rounded-circle w-100">
         </div>
         <div class="col-9 pt-5">
                 <div class="d-flex justify-content-between align-items-baseline">
-                    <h1><?php echo e($user->username); ?></h1>
-                    <a href="#">Add a post..</a>
+                    
+                    <div class="d-flex align-items-center pb-3">
+                        <div class="h4"><?php echo e($user->username); ?></div>
+                        <button class="btn btn-primary ml-4">Follow</button>
+                    </div>
+
+                    <?php if (app(\Illuminate\Contracts\Auth\Access\Gate::class)->check('update', $user->profile)): ?>
+                        <a href="/p/create">Add new post..</a>
+                    <?php endif; ?>
+
                 </div>
+
+                <?php if (app(\Illuminate\Contracts\Auth\Access\Gate::class)->check('update', $user->profile)): ?>
+                    <a href="/profile/ <?php echo e($user->id); ?>/edit">Edit Profile</a>
+                <?php endif; ?>
+                
                 <div class="d-flex">
-                    <div class="pr-5"><strong>153</strong> posts</div>
+                <div class="pr-5"><strong><?php echo e($user->posts->count()); ?></strong> posts</div>
                     <div class="pr-5"><strong>23k</strong> followers</div>
                     <div class="pr-5"><strong>212k</strong> following</div>
                 </div>
@@ -21,10 +34,14 @@
 
     </div>
 
-    <div class="row pt-5">
-        <div class="col-4"><img src="https://instagram.fhre2-1.fna.fbcdn.net/v/t51.2885-15/e35/c1.0.748.748a/s150x150/82339013_604012433755999_687788727040640519_n.jpg?_nc_ht=instagram.fhre2-1.fna.fbcdn.net&_nc_cat=107&_nc_ohc=mDeDe1uXsS4AX9gcNv8&oh=86c1740b2087ea82de533158a3e81939&oe=5EBE8605" class="w-100"></div>
-        <div class="col-4"><img src="https://instagram.fhre2-1.fna.fbcdn.net/v/t51.2885-15/e35/c0.111.928.928a/s480x480/82967728_222124025469517_5532436576170466152_n.jpg?_nc_ht=instagram.fhre2-1.fna.fbcdn.net&_nc_cat=101&_nc_ohc=LWLpPH3_9rwAX8GG5z9&oh=41792d0fed6ccf7c4e4da7c8ae410360&oe=5EBA29F9" class="w-100"></div>
-        <div class="col-4"><img src="https://instagram.fhre2-1.fna.fbcdn.net/v/t51.2885-15/e35/c95.0.560.560a/s150x150/83685533_642392923240008_2402831147948054363_n.jpg?_nc_ht=instagram.fhre2-1.fna.fbcdn.net&_nc_cat=100&_nc_ohc=4R17LlZsjLAAX8cfOXt&oh=228b22f59d333573c1b7e9c6174e4a28&oe=5EC080A6" class="w-100"></div>
+    <div class="row pt-5 ">
+        <?php $__currentLoopData = $user->posts; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $post): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+            <div class="col-4 pb-4">
+            <a href="/p/<?php echo e($post->id); ?>">
+                    <img src="/storage/<?php echo e($post->image); ?>" class="w-100">
+                </a>
+            </div>
+        <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
     </div>
 
 </div>
